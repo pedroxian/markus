df82fe8a908068b0e0274e0b35b267a3d0168efc
pedroxian <44950875+pedroxian@users.noreply.github.com>
2021-09-19 20:20:51 +0200

    comitter

---
+++ colour.bat
@@ -0,0 +1,12 @@
+astend.protocol .for -even.hash tode.root mash pop.drop /hacker
+
+    else.do faction.grey -opec radion.online =ospot.ratio
+
+        mash.give otopous .ratpoison .rope /made.from.africa
+
+
+microwave.morphing event.singularity ofset.bat
+
+    end.script do:factory
+
+    pause
